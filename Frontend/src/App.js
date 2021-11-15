120643fe8d85e2195600ec419d4a478d6a7becd1
Gustavo <gugahh.br@gmail.com>
2021-11-15 12:16:10 -0300

    Inicialização do Bootstrap

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from "react-bootstrap/Button";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div class="container" class="fixed-top">
+        <img src="imgs/logo_the_dudes_big.png" alt="logo the Dudes" />
+      </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+        <Button variant="primary">Bootstrap Button</Button>
         </p>
         <a
           className="App-link"
